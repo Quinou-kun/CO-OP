22bd1b8f39a52d982b5525316b5a54523915fe1e
RENOUARD Quentin <quentin.renouard9@etu.univ-lorraine.fr>
2018-01-15 14:01:28 +0100

    Initial commit

---
+++ src/components/Signin.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>Signin</h1>
+        <form @submit.prevent="signin()">
+            <div>
+                <label for="email">Email</label>
+                <input type="text" id="email" placeholder="email" v-model="user.email"/>
+            </div>
+            <div>
+                <label for="email">Password</label>
+                <input type="password" id="password" placeholder="password" v-model="user.password"/>
+            </div>
+            <input type="submit" value="Go !">
+        </form>
+    </div>
+</template>
+
+<script>
+  import api from '../api'
+  export default {
+    name: 'Signin',
+    data () {
+      return {
+        user: {email: '', password: ''}
+      }
+    },
+    methods: {
+      signin () {
+        api.post('/members/signin', this.user).then((response) => {
+          console.log(response.data)
+        })
+      }
+    }
+  }
+</script>
---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+export default {
+  remoteURL: 'http://coop.api.netlor.fr/api',
+  apiKey: 'ee09702a34ff4ea293a27908194cbc3e'
+}
